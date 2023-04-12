5a3715f20fc74bb208ecbc2f69a21d882734f074
salehmu <ghd@keemail.me>
2023-04-12 07:15:57 +0200

    Fix marking

--- .github/p.py
+++ .github/p.py
@@ -16,7 +16,7 @@ image_height = (num_rows * cell_height) * 4 + (table_margin * 2) + (table_spacin
 image = Image.new("RGB", (image_width, image_height), (253, 253, 253))
 draw = ImageDraw.Draw(image)
 
-file_list = sorted(os.listdir("../src"))
+file_list = sorted([f for f in os.listdir("../src") if not f.endswith('.png')])
 
 prev_num = None
 missing_files = []
@@ -47,9 +47,6 @@ for i in range(4):
             elif str(num).zfill(4) + ".l" in file_list:
                 draw.rectangle((x+1, y+1, x+cell_width-1, y+cell_height-1), fill=(0, 153, 0))
                 draw.text((x+2, y+2), str(num), font=font, fill=(255, 255, 255))
-            elif str(num).zfill(4) + ".l" in os.listdir("../skt"):
-                draw.rectangle((x+1, y+1, x+cell_width-1, y+cell_height-1), fill=(255, 0, 0))
-                draw.text((x+2, y+2), str(num), font=font, fill=(0, 0, 0))
             else:
                 draw.text((x+2, y+2), str(num), font=font, fill=(0, 0, 0))
 
@@ -61,4 +58,16 @@ for i in range(4):
                         image_width - table_margin, y_offset + num_rows * cell_height + table_spacing),
                        fill=(255, 255, 255))
 
-image.save("progress.png")
+# mark files in ../skt with red
+# mark files in ../skt with red
+for filename in os.listdir("../skt"):
+    if filename.endswith(".l"):
+        num = int(filename.split(".")[0])
+        if num <= num_cols * num_rows * 4:
+            x_offset = table_margin + ((num-1) % num_cols) * cell_width
+            y_offset = table_margin + ((num-1) // (num_cols * num_rows)) * ((num_rows * cell_height) + table_spacing)
+
+            draw.rectangle((x_offset+1, y_offset+1, x_offset+cell_width-1, y_offset+cell_height-1), fill=(255, 0, 0))
+            draw.text((x_offset+2, y_offset+2), str(num), font=font, fill=(0, 0, 0))
+
+image.save("../src/p.png")
